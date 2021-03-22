f3e288aeadfd0413a282b84cd2a19af0d51f17eb
YM-GitHub-Personal <72395326+YM-GitHub-Personal@users.noreply.github.com>
2021-03-22 20:51:39 +0900

    課題１：簡単な検索ツールの作成
    課題１～４の回答

---
+++ search_2.py
@@ -0,0 +1,24 @@
+
+### 検索ツールサンプル
+### これをベースに課題の内容を追記してください
+
+# 検索ソース
+source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
+
+### 検索ツール
+def search():
+    word = input("鬼滅の登場人物の名前を入力してください >>> ")
+    
+    ### ここに検索ロジックを書く
+    for i in range(7):
+        if word == source[i]:
+            print("{}が見つかりました".format(word))
+        else:
+            print("{}は見つかりませんでした。".format(word))
+            print("{}を追加します".format(word))
+            source.append(word)
+            for app in range(8):
+                print(source[app])
+
+if __name__ == "__main__":
+    search()
---
+++ search_3.py
@@ -0,0 +1,33 @@
+
+### 検索ツールサンプル
+### これをベースに課題の内容を追記してください
+
+# 検索ソース
+source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
+import csv
+### 検索ツール
+def search():
+    word =input("鬼滅の登場人物の名前を入力してください >>> ")
+    
+    ### ここに検索ロジックを書く
+    # csvファイルを指定
+    csvFile = 'C:\Virsual Studio Code\MyPython\GitHub_DownLoad\study-01-search-main\search.csv'
+
+    # チェックフラグ
+    cheFlg = 0
+    # csvファイルオープン
+    with open(csvFile, mode = 'r') as r:
+        reader = csv.reader(r)
+        for row in reader:
+            if word == row:
+                cheFlg = cheFlg + 1
+                print("{}が見つかりました".format(word))
+        
+        if cheFlg == 0:
+            print("{}は見つかりませんでした。".format(word))
+            print("{}を追加します".format(word))
+            source.append(word)
+            print(source)
+
+if __name__ == "__main__":
+    search()
